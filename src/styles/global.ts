abf53e1cce94f269eb1c7bad6a44c571a5612314
Kiérico Souza <kiericograndao@gmail.com>
2023-02-21 16:37:59 -0400

    #2.2 - Estilos globais

---
+++ src/styles/global.ts
@@ -0,0 +1,20 @@
+import { globalCss } from ".";
+
+export const globalStyles = globalCss({
+    '*': {
+        margin: 0,
+        padding: 0,
+    },
+
+    body: {
+        backgroundColor: "$grayBackground900",
+        color: "$grayText300",
+        '-webkit-font-smoothing': 'antialiased',
+    },
+
+    'body, input, textarea, button': {
+        fontFamily: "'Roboto', sans-serif",
+        fontWeight: 400,
+
+    }
+})
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -13,6 +13,16 @@ export const {
     theme: {
         colors: {
             purple: '#8257e6',
+
+            white: '#fff',
+
+            grayTitle100: '#e1e1e6',
+            grayText300: '#c4c4cc',
+            grayElements800: '#202024',
+            grayBackground900: '#121214',
+
+            greenLight300: '#00b37e',
+            green500: '#00875f',
         }
     }
 })
