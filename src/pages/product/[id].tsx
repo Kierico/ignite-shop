99d59ae5a77fa5963520fde4cc8354a7b50894fa
Kiérico Souza <kiericograndao@gmail.com>
2023-02-22 14:45:02 -0400

    #4.3 - Carregando dados do produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,22 +1,66 @@
-import { useRouter } from "next/router";
+import { GetStaticProps } from "next";
+import Stripe from "stripe";
+import Image from "next/image";
+import { stripe } from "@/src/lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
 
-export default function Product() {
-    const { query } = useRouter()
+interface ProductProps {
+    product: {
+        id: string;
+        name: string;
+        imageUrl: string;
+        price: string;
+        description: string;
+    }
+}
+
+export default function Product({ product }: ProductProps) {
 
     return (
         <ProductContainer>
             <ImageContainer>
-
+                <Image
+                    src={product.imageUrl}
+                    alt=""
+                    width={520}
+                    height={480}
+                />
             </ImageContainer>
             <ProductDetails>
-                <h1>Camiseta X</h1>
-                <span>R$ 79,90</span>
-                <p>Gostosas como um abraço, nossos produtos são feitos do mais puro e nobre algodão brasileiro, ideais para climas de Norte a Sul. Todas as cores são 100% algodão, exceto as cinzas-mescla, que são 88% algodão e 12% poliéster.</p>
+                <h1>{product.name}</h1>
+                <span>{product.price}</span>
+                <p>{product.description}</p>
                 <button>
                     Comprar agora
                 </button>
             </ProductDetails>
         </ProductContainer>
     )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+
+    const productId = params.id;
+
+    const product = await stripe.products.retrieve(productId, {
+        expand: ['default_price'],
+    });
+
+    const price = product.default_price as Stripe.Price // para entender o expandable do 'default.price'.
+
+    return {
+        props: {
+            product: {
+                id: product.id,
+                name: product.name,
+                imageUrl: product.images[0], // só pega a primeira imagem no array.
+                price: new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                }).format(price.unit_amount / 100), // salva em centavos, para não ter problemas com vírgula (float).
+                description: product.description,
+            }
+        },
+        revalidate: 60 * 60 * 1, // 1 hour
+    }
 }
