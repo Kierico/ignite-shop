d1eea2f90ec3b1e81da8b2b08432172c87260517
Kiérico Souza <kiericograndao@gmail.com>
2023-02-22 18:35:27 -0400

    #4.11 - Dados da compra no sucesso

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -14,8 +14,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(400).json({ error: 'Price not fond.' })
     }
 
-    const success_url = `${process.env.NEXT_URL}/success`
-    const cancel_url = `${process.env.NEXT_URL}/`
+    const success_url = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
+    const cancel_url = `${process.env.NEXT_URL}/`;
 
     const checkoutSession = await stripe.checkout.sessions.create({
         success_url: success_url, // url para direcionar o cliente depois da compra.
@@ -31,5 +31,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     return res.status(201).json({
         checkoutUrl: checkoutSession.url,
-    })
+    });
 }
