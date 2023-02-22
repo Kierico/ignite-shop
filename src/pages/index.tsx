067b9c2c1cff466fff400495f91e13cc929e2d0e
Kiérico Souza <kiericograndao@gmail.com>
2023-02-22 13:06:06 -0400

    #3.5 - Formatano preço

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -72,7 +72,10 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0], // só pega a primeira imagem no array.
-      price: price.unit_amount / 100, // salva em centavos, para não ter problemas com vírgula (float).
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount / 100), // salva em centavos, para não ter problemas com vírgula (float).
     }
   })
 
